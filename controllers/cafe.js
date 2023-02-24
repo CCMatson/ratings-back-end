8dca8443df3f22ccbd8bb8ea05b1db29c069678a
CCMatson <catherine.matson@gmail.com>
2023-02-23 20:07:02 -0800

    create cafe model

---
+++ controllers/cafe.js
@@ -0,0 +1,5 @@
+const { Cafe } = require("../models")
+
+module.exports = {
+  
+}
---
+++ routes/cafes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const cafesCtrl = require('../controllers/cafes.js')
+
+
+
+module.exports = router
